240702c4ab28d0c1ee0d88a16a57774cb3074f60
sandeep <9041128179sandy@gmail.com>
2020-06-06 21:57:22 +0530

    done with basic project setup

---
+++ ExtendReportsTest/src/test/java/basePackage/TestBase.java
@@ -0,0 +1,12 @@
+package basePackage;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class TestBase {
+	
+	public static final Logger logger = LogManager.getLogger(TestBase.class);
+	
+
+}
---
+++ ExtendReportsTest/src/test/java/testcases/LoginTest.java
@@ -0,0 +1,30 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+import basePackage.TestBase;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class LoginTest extends TestBase {
+  @Test
+  public void loginTest() {
+	  System.out.println("Hello world");
+	  
+	  logger.debug("this is debug message");
+	  logger.info("this is information message");
+	  logger.error("this is the error log message");
+	  
+	  
+	
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
